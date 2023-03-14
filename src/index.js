9270b6c269eb19989df4a56d89fce6b7077449bb
I am Alaska <stephen.alaska94@hotmail.com>
2023-03-14 09:23:17 +0000

    refactor the html homepage to js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './styles.css';
 import './assets/movieverse-logo.png';
+import { homePage } from './modules';
+
+const mainContainer = document.querySelector('main');
+
+window.addEventListener('DOMContentLoaded', () => {
+  mainContainer.innerHTML = homePage();
+});
---
+++ src/modules/index.js
@@ -0,0 +1,1 @@
+export { default as homePage } from './homepage.js';
